cf1b6a39bfe9c30a2483fffca8d49009be8a18c0
Tyler Geerdts <tygee713@gmail.com>
2022-11-14 19:14:28 -0500

    Added solid setup and first components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header/index.jsx'
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <Map /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+function Header() {
+
+  return (
+    <div class='background'>
+      <h1 class='headerText'>IP Address Tracker</h1>
+    </div>
+  )
+}
+
+export default Header
